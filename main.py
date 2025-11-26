c1a4366f6a075b4186722c9c4a089a14a5b32eac
mhuelsbeck2 <mhuelsbeck2@gmail.com>
2025-11-26 14:44:41 -0500

    add formatting to output

--- main.py
+++ main.py
@@ -6,9 +6,15 @@ def get_book_text(filepath):
         return f.read()
 
 def main():
-    word_count = get_word_count(get_book_text("books/frankenstein.txt"))
-    char_count = get_char_count(get_book_text("books/frankenstein.txt"))
+    filepath = "books/frankenstein.txt"
+    word_count = get_word_count(get_book_text(filepath))
+    char_count = get_char_count(get_book_text(filepath))
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
     print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
     print(char_count)
+    print("============= END ===============")
 
 main()
